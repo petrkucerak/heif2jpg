d860d2648d7e7d00e83a7e83462893a9e1da556e
petrkucerak <petr@khome.cz>
2022-09-01 14:49:19 +0200

    Add script

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+command -v "heif-convert" >/dev/null 2>&1 || { echo "It requires heif-convert, but it is not installed, or not on the path."; exit 1; }
+
+for file in "$@"
+do
+    filename=$(basename "$file")
+    filename_without_extension="${filename%.*}"
+    
+    jpeg_filename="${filename_without_extension}.jpg"
+    
+    echo "Converting $file to $jpeg_filename"
+    
+    heif-convert "$filename" "$jpeg_filename"
+done
